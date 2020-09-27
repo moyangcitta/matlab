c6c833bbdb017cda9320361e912f82a2dc3582de
moyangcitta <2033402740@qq.com>
2020-09-27 16:25:05 +0800

    project

--- Inverted_Pendulum.m
+++ Inverted_Pendulum.m
@@ -2,8 +2,8 @@ M = 2;
 m = 0.1;
 l = 0.5;
 g = 9.81;
-A = [0 1 0 0;0 0 -1*m*g/M 0;0 0 0 1;0 0 -1*m*g/M 0]
-B = [0;-1/M;0;-1/(M*l)]
+A = [0 1 0 0;0 0 -1*m*g/M 0;0 0 0 1;0 0 (m+M)*g/(M*l) 0]
+B = [0;1/M;0;-1/(M*l)]
 C = [1 0 0 0 ];
 Qc = ctrb(A,B);
 rank_Qc = rank(Qc);
@@ -15,5 +15,35 @@ if rank_Qc == h
     if rank_Qo == h
         P_A = poly(A);
         a4 = P_A(5);a3 = P_A(4);a2 = P_A(3);a1 = P_A(2);
+        K1 = [-88.16 -55.93 -212.68 -47.96];
+        K2 = -88.16;
+        AA = [A-B*K1 B*K2;-C 0];
+        BB = [zeros(4,1);1];
+        CC = [C 0];
+        DD = 0;
+        t = 0:0.01:8;
+        [y,X,t] = step(AA,BB,CC,DD,1,t);
+        x1 = X(:,1);
+        x2 = X(:,2);
+        x3 = X(:,3);
+        x4 = X(:,4);
+        w = X(:,5);
+        subplot(3,2,1)
+        plot(t,x1,'r'),grid
+        xlabel('t(sec)'),ylabel('x1')
+        subplot(3,2,2)
+        plot(t,x2,'k'),grid
+        xlabel('t(sec)'),ylabel('x2')
+        subplot(3,2,3)
+        plot(t,x3,'k'),grid
+        xlabel('t(sec)'),ylabel('x3')
+        subplot(3,2,4)
+        plot(t,x4,'k'),grid
+        xlabel('t(sec)'),ylabel('x4')
+        subplot(3,2,5)
+        plot(t,w,'k'),grid
+        xlabel('t(sec)'),ylabel('w')
+    end
+end
         
     
